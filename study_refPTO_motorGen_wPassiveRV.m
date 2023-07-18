4baede116b61b77bdbfaebcbc6076d0ab00f3db0
Jeremy Simmons <simmo536@umn.edu>
2023-07-18 16:46:33 -0500

    add design study for sizing hdyraulic pump/motor driving the electric generator

---
+++ study_refPTO_motorGen_wPassiveRV.m
@@ -0,0 +1,171 @@
+% study_refPTO_motorGen_wPassiveRV.m script m-file
+% AUTHORS:
+% Jeremy Simmons (email: simmo536@umn.edu)
+% University of Minnesota
+% Department of Mechanical Engineering
+%
+% CREATION DATE:
+% 7/18/2023
+%
+% PURPOSE/DESCRIPTION:
+% This script performs parameter variation studies
+% using the model contained in sys_refPTO.m and solved by
+% sim_refPTO.m.
+% The parameter initiallization functions are called within this
+% script before the sim_refPTO.m script is called.
+%
+% This specific script studies the size of hydraulic motor driving the 
+% electric generator.
+%
+% This script is set up to be run as part of a SLURM job array. The
+% following lines are required before this script is called:
+%   iVar = ${SLURM_ARRAY_TASK_ID};
+%   SS=1;
+%
+% FILE DEPENDENCY:
+% ./Reference PTO/
+%   initialConditionDefault_refPTO
+%   parameters_refPTO.m
+%   sim_refPTO.m
+%   stateIndex_refPTO.m
+%   sys_refPTO.m
+% ./WEC model/
+%   flapModel.m
+%   hydroStaticTorque.m
+%   parameters_WECmodel.m
+% ./WEC model/WECdata
+%   nemohResults_vantHoff2009_20180802.mat
+%   vantHoffTFCoeff.mat
+% ./Solvers/
+%   deltaE_NI.m
+%   deltaV_NI.m
+%   ode1.m
+% ./Components/
+%   areaFracPWM.m
+%   capAccum.m
+%   deadVCap.m
+%   flowCV.m
+%   flowPRV.m
+% ./Utilities/
+%   statsTimeVar_cdf.m
+%
+% UPDATES:
+% 7/18/2023 - Created from study_refPTO_accum_woRV.m
+%
+% Copyright (C) 2023  Jeremy W. Simmons II
+% 
+%   This program is free software: you can redistribute it and/or modify
+%   it under the terms of the GNU General Public License as published by
+%   the Free Software Foundation, either version 3 of the License, or
+%   (at your option) any later version.
+% 
+%   This program is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%   GNU General Public License for more details.
+% 
+%   You should have received a copy of the GNU General Public License
+%   along with this program. If not, see <https://www.gnu.org/licenses/>.
+%
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% clear
+% clc
+addpath('WEC model') 
+addpath(['WEC model' filesep 'WECdata']) 
+addpath('Reference PTO')
+addpath('Components')
+addpath('Sea States')
+addpath('Solvers')
+addpath('Utilities')
+%% %%%%%%%%%%%%   SIMULATION PARAMETERS  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Simulation timeframe
+par.Tramp = 250; % [s] excitation force ramp period
+par.tstart = 0; %[s] start time of simulation
+par.tend = 500; %[s] end time of simulation
+
+% Solver parameters
+% par.odeSolverRelTol = 1e-4; % Rel. error tolerance parameter for ODE solver
+% par.odeSolverAbsTol = 1e-4; % Abs. error tolerance parameter for ODE solver
+par.MaxStep = 5e-5;             % [s] for fixed time solver, this is the step size for solver
+par.downSampledStepSize = 1e-2; % [s] specifies time step for data output
+if mod(par.downSampledStepSize,par.MaxStep)
+    warning('down-sampled time step is not an integer multiple of the maximum step size')
+end
+
+% Sea State and Wave construction parameters
+Hs = [2.34 2.64 5.36 2.05 5.84 3.25];
+Tp = [7.31 9.86 11.52 12.71 15.23 16.5];
+par.wave.Hs = Hs(SS);
+par.wave.Tp = Tp(SS);
+par.WEC.nw = 1000; % num. of frequency components for harmonic superposition
+par.wave.rngSeedPhase = 3; % seed for the random number generator
+
+% load parameters
+par = parameters_refPTO(par,...
+    'nemohResults_vantHoff2009_20180802.mat','vantHoffTFCoeff.mat');
+
+% Define initial conditions
+stateIndex_refPTO % load state indices, provides 'iy_...'
+initialConditionDefault_refPTO % default ICs, provides 'y0'
+
+%% Special modifications to base parameters
+% par.Sro = 3000; % [m^3]
+% par.D_WEC = 0.3;         % [m^3/rad] flap pump displacement
+p_ro_nom = [5e6 7e6 8e6 7e6 8e6 8e6]; % [Pa]
+par.control.p_ro_nom = p_ro_nom(SS);
+par.duty_sv = 0;
+
+% Configuration
+par.ERUconfig.present = 1;
+par.ERUconfig.outlet = 1;
+
+par.rvConfig.included = 1; % RO inlet valve is 1 - present, 0 - absent
+par.rvConfig.active = (0)*par.rvConfig.included; % RO inlet valve is 1 - active, 0 - passive
+% dp_rated = 1e5; % [Pa] 
+% q_rated = 1000e-3; % [(lpm) -> m^3/s]
+% par.kv_rv = q_rated/dp_rated;
+par.kv_rv = (0.0047)/sqrt(1e3); % [(L/s/kPa^0.5) -> m^3/s/Pa^0.5]
+
+par.Vc_h = (5000)*1e-3; % [(L) -> m^3] gas volume at charge pressure
+par.Vc_ro = (5000)*1e-3; % [(L) -> m^3] gas volume at charge pressure
+
+par.w_pm_max = (1750)/60*2*pi; % [(rpm) -> rad/s] maximum speed of motor
+
+%% %%%%%%%%%%%%   Study Variables  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% motor/pump displacement
+Dpm = [100 200 400 600 800 1000]*1e-6/(2*pi);% [L->m^3] total accumulator volume
+nVar = numel(Dpm);
+
+saveSimData = 1; % save simulation data (1) or just output variables (0)
+
+%% %%%%%%%%%%%%   COLLECT DATA  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% change design parameter
+par.D_pm = Dpm(iVar);
+
+% run simulation
+ticSIM = tic;
+out = sim_refPTO(y0,par);
+toc(ticSIM)
+
+if ~saveSimData
+    clear out
+end
+
+
+
+%% %%%%%%%%%%%%   Save Data  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+timeStamp = datestr(now,'yyyymmddTHHMMSS'); % time in ISO8601
+
+% Save data
+filename = ['data_refPTO_motorGen_wPassiveRV_', ...
+            datestr(now,'yyyymmdd'),'_',num2str(SS),'_' ...
+            num2str(iVar)];
+save(filename)
+
+%% %%%%%%%%%%%%   End Computations  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+poolobj = gcp('nocreate'); delete(poolobj);
+
+return
---
+++ study_refPTO_motorGen_wPassiveRV.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --mem=16gb
+#SBATCH -t 4:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=simmo536@umn.edu
+#SBATCH -p small
+#SBATCH -o %A_%a.out
+#SBATCH -e %A_%a.err
+
+cd ~/2023-NSF_CPS-wavePoweredRO
+module load matlab
+matlab -nodisplay -r \
+"iVar = ${SLURM_ARRAY_TASK_ID}; \
+SS = $SS; \
+study_refPTO_motorGen_wPassiveRV"
+
+# Commands to use
+# sbatch --export=SS=1 --array=1-15 ~/2023-NSF_CPS-wavePoweredRO/study_refPTO_motorGen_wPassiveRV.sh
+# dos2unix  study_refPTO_accum_woRV.sh
+
