7f611a036b40542a9d3bcfe6718873e00b88b887
Jeremy Simmons <simmo536@umn.edu>
2023-07-18 17:10:31 -0500

    add pareto front analysis, correct volume units and scaling, add sea state to titles, normalize power losses

--- postprocess_datafiles_accum_wRV.m
+++ postprocess_datafiles_accum_wRV.m
@@ -20,7 +20,6 @@ display(['file ',num2str(j),' of ',num2str(nfiles)])
 end
 
 %% Find indices for missing data files
-if 1
 
 files = dir;
 nfiles = size(files,1);
@@ -70,9 +69,6 @@ catch
     % just move on
 end
 
-end
-
-
 
 %% Transform data to 3D variable mesh
 I = length(kv);
@@ -113,19 +109,19 @@ clearvars test
 %% Plot average power loss from the ripple control valve as a function of total accumulator volume for distribution (color) and valve coefficient (line type)
  % select indices to plot
     % distribution
-    iiK = 1:4:K;
+    iiK = 3:1:K-2;
     nK = length(iiK);
     % valve coeff.
-    iiI = 1:2:I;
+    iiI = 3:1:I;
     nI = length(iiI);
    
   % select variable to plot
   switch 1
       case 1
-        Y = PP_rv_3D;
+        YaxisVar = 100*PP_rv_3D./PP_WEC_3D;
         varStr = 'Ripple Control Valve Losses';
       case 2
-        Y = PP_roPRV_3D + PP_hPRV_3D;
+        YaxisVar = 100*(PP_roPRV_3D + PP_hPRV_3D)./PP_WEC_3D;
         varStr = 'PRV Losses';
   end
 
@@ -137,7 +133,7 @@ orange = [226 100 55]/256;
 green = [63 150 87]/256;
 color = [maroon; gold; blue; orange; green];
 
-linestyles = {'-', '--', ':', '-.'};
+linestyles = {'-', '--', '-.', ':','-', '--', '-.', ':',};
 
 bottomEdge = 1;
 leftEdge = 3;
@@ -183,7 +179,7 @@ end
 
 for k = 1:nK
     for i = 1:nI
-        p(k,i) = plot(Vtotal*1e3,1e-3*Y(iiI(i),:,iiK(k)), ...
+        p(k,i) = plot(Vtotal,YaxisVar(iiI(i),:,iiK(k)), ...
             'LineStyle', linestyles{i}, ...
             'Color',color(k,:), ...
             'LineWidth',1);
@@ -191,9 +187,9 @@ for k = 1:nK
     end
 end
 
-xlabel('volume (L)', ...
+xlabel('volume (1000L) ', ...
 'Interpreter','latex','FontSize',fontSize-1,'fontname','Times')
-ylabel('power (kW)', ...
+ylabel('power loss (x100\%)', ...
 'Interpreter','latex','FontSize',fontSize-1,'fontname','Times')
 switch par.ERUconfig.outlet
     case 1
@@ -208,7 +204,9 @@ switch par.rvConfig.active
         rvStr = 'Active Ripple Control';
 end
 
-titleString = ['Mean Power Loss With ',rvStr,ERUstr,': ',varStr];
+titleString = ['Mean Power Loss Normalized to Mean Power Capture',newline...
+                'With ',rvStr,ERUstr,': ',varStr,newline,...
+                'Sea State ',num2str(SS)];
 title(titleString,...
 'Interpreter','latex','FontSize',fontSize,'fontname','Times')
 
@@ -219,27 +217,28 @@ rect = [0.5, -0.2, 0.25, 0.15];
 % set(leg, 'Position', rect)
 set(leg, 'Location', 'best')
 % set(leg, 'Location', 'southoutside')
-xlim([0 1e3*max(Vtotal)])
+xLim = xlim;
+xlim([0 xLim(2)])
 % ylim([0 1e-3*max(Y(:))])
 
 
 %% Plot rate of change as a function of total accumulator volume for distribution (color) and valve coefficient (line type)
  % select indices to plot
     % distribution
-    iiK = 1:4:K;
+    iiK = 3:1:K-2;
     nK = length(iiK);
     % valve coeff.
-    iiI = 1:2:I;
+    iiI = 3:1:I;
     nI = length(iiI);
    
   % select variable to plot
   maxOr97 = 1;
   switch maxOr97
       case 1
-        Y = dpdt_max_3D;
+        YaxisVar = dpdt_max_3D;
         varTitle = 'Maximum Rate of Change in Pressure';
       case 2
-        Y = dpdt_97_3D;
+        YaxisVar = dpdt_97_3D;
         varTitle = '97th Percentile Rate of Change in Pressure';
   end
 
@@ -251,7 +250,7 @@ orange = [226 100 55]/256;
 green = [63 150 87]/256;
 color = [maroon; gold; blue; orange; green];
 
-linestyles = {'-', '--', ':', '-.'};
+linestyles = {'-', '--', '-.', ':','-', '--', '-.', ':',};
 
 bottomEdge = 1;
 leftEdge = 3;
@@ -297,7 +296,7 @@ end
 
 for k = 1:nK
     for i = 1:nI
-        p(k,i) = plot(Vtotal*1e3,1e-3*Y(iiI(i),:,iiK(k)), ...
+        p(k,i) = plot(Vtotal,1e-3*YaxisVar(iiI(i),:,iiK(k)), ...
             'LineStyle', linestyles{i}, ...
             'Color',color(k,:), ...
             'LineWidth',1);
@@ -307,11 +306,11 @@ end
 
 % plot target limit
 iLeg = iLeg+1;
-plot(Vtotal([1 end])*1e3,1e-3*par.control.dpdt_ROmax*[1 1])
+plot(Vtotal([1 end]),1e-3*par.control.dpdt_ROmax*[1 1])
 legLabels(iLeg) = convertCharsToStrings( ...
         ['target limit']);
 
-xlabel('volume (L)', ...
+xlabel('volume (1000L) ', ...
 'Interpreter','latex','FontSize',fontSize-1,'fontname','Times')
 ylabel('rate of change in pressure (kPa/s)', ...
 'Interpreter','latex','FontSize',fontSize-1,'fontname','Times')
@@ -328,7 +327,8 @@ switch par.rvConfig.active
         rvStr = 'Active Ripple Control';
 end
 
-titleString = [varTitle,' With ',rvStr,ERUstr];
+titleString = [varTitle,' With ',rvStr,ERUstr,':',newline,...
+                'Sea State ',num2str(SS)];
 title(titleString,...
 'Interpreter','latex','FontSize',fontSize,'fontname','Times')
 
@@ -339,13 +339,19 @@ rect = [0.5, -0.2, 0.25, 0.15];
 % set(leg, 'Position', rect)
 set(leg, 'Location', 'best')
 % set(leg, 'Location', 'southoutside')
-xlim([0 1e3*max(Vtotal)])
-ylim([0 1e-3*1.5*par.control.dpdt_ROmax])
+xLim = xlim;
+xlim([0 xLim(2)])
+yLim = ylim;
+ylim([0 yLim(2)])
+% ylim([0 1e-3*1.5*par.control.dpdt_ROmax])
 
-%% Plot power loss versus total accumulator volume for designs meeting threshhold on rate of pressure change
-X = Vtotal_mesh';
-Y = 1e-3*PP_rv_array;
+%% Plot power loss versus total accumulator volume for pareto optimal 
+% designs meeting threshhold on rate of pressure change
+
+XaxisVar = Vtotal_mesh';
+YaxisVar = 100*PP_rv_array./PP_WEC_array;
 bounds = [1.001 1 0.90 0.75 0.5 0];
+N = numel(bounds)-1; % Number of bins with bounds
 dpdt_ub = par.control.dpdt_ROmax*bounds;
 maxOr97 = 2;
 switch maxOr97
@@ -359,6 +365,54 @@ switch maxOr97
         varLabel = '97th Percentile';
 end
 
+
+
+iDomStart = ones(1,N+1);
+iDom = [];
+% Loop through bounds
+for k = 1:N
+    % Find individuals meeting dpdt bounds
+    ub = dpdt_ub(k);
+    lb = dpdt_ub(k+1);
+    [~,meetsConstraints] = find(dpdt_metric <= ub);
+
+    % load objectives
+    obj1 = -XaxisVar(meetsConstraints);
+    obj2 = -YaxisVar(meetsConstraints);
+
+    % Number of individuals
+    n = numel(obj1);
+
+    % Initialize dominance matrix
+    dominance = false(n);
+    
+    % figure
+    % scatter(obj1,obj2)
+    % hold on
+    % Find and mark dominating individuals meeting dpdt criterion
+    for i = 1:n
+        for j = 1:n
+            if obj1(i) >= obj1(j) && obj2(i) >= obj2(j) ...
+               && (obj1(i) > obj1(j) || obj2(i) > obj2(j))
+                dominance(i,j) = true;
+            end
+        end
+    end
+    
+    % Identify non-dominated individuals
+    non_dominated = find(sum(dominance, 1) == 0)
+
+    % scatter(obj1(non_dominated),obj2(non_dominated))
+
+    % Sort non-dominated individuals based on objective values
+    [~, sort_idx] = sort(-obj1(non_dominated));
+    Ndom = numel(non_dominated);
+
+    iDomStart(k+1) = iDomStart(k) + Ndom;
+    iDom = [iDom meetsConstraints(non_dominated(sort_idx))];
+end
+iDomStart(k+1) = numel(iDom)+1;
+
 black = [0 0 0];
 maroon = [122 0 25]/256;
 gold = [255 204 51]/256;
@@ -366,11 +420,12 @@ blue = [0 75 135]/256;
 orange = [226 100 55]/256;
 green = [63 150 87]/256;
 color = [maroon; gold; blue; orange; green];
+markerType = '.ox*^s';
 
 bottomEdge = 1;
 leftEdge = 3;
 width = 7.5; % one column: 3+9/16, two column: 7.5
-height = 3.25;
+height = 6;
 fontSize = 9;
 lineWidth = 1;
 
@@ -380,30 +435,33 @@ fig = figure;
 fig.Units = 'inches';
 fig.Position = [leftEdge bottomEdge width height ];
 
-n_plots = 1;
-ax1 = subplot(n_plots,1,1);
-ax1.FontName = 'times';
-ax1.FontSize = fontSize-1;
+n_plots = 3;
 
-N = length(dpdt_ub)-1;
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+iax = 1;
+ax(iax) = subplot(n_plots,1,iax);
+ax(iax).FontName = 'times';
+ax(iax).FontSize = fontSize-1;
+
+
 iLeg = 0;
-for i = N:-1:1
-    ub = dpdt_ub(i)
-    lb = dpdt_ub(i+1)
-    iVar = find((dpdt_metric <= ub) & (dpdt_metric > lb));
-    s = scatter(X(iVar),Y(iVar),100,[color(N-i+1,:)])
-    s.Marker = '*';
-    s.LineWidth = 1.5;
+for i = 1:N
+    iVar = iDom(iDomStart(i):iDomStart(i+1)-1);
+    p(iax,i) = plot(XaxisVar(iVar),YaxisVar(iVar));
+    p(iax,i).Color = [color(i,:)];
+    p(iax,i).Marker = markerType(i);
+    p(iax,i).MarkerSize = 5;
+    p(iax,i).LineWidth = 1.5;
     hold on
 
     iLeg = iLeg+1;
     legLabels(iLeg) = convertCharsToStrings( ...
-        ['dp/dt|_{',varLegLabel,'} <= ',num2str(bounds(i)),'*dp/dt|_{limit}']);
+        ['dp/dt|_{',varLegLabel,'} <= ',num2str(1e-3*dpdt_ub(i),3),' kPa/s']);
 end
 
-xlabel('volume (L)', ...
+xlabel('volume (1000L) ', ...
 'Interpreter','latex','FontSize',fontSize-1,'fontname','Times')
-ylabel('power loss (kW)', ...
+ylabel('power loss (x100\%)', ...
 'Interpreter','latex','FontSize',fontSize-1,'fontname','Times')
 switch par.ERUconfig.outlet
     case 1
@@ -418,7 +476,9 @@ switch par.rvConfig.active
         rvStr = 'Active Ripple Control';
 end
 
-titleString = ['Power Loss With ',rvStr,ERUstr,': ',varLabel,' Rate of Pressure Change Compared to Limit'];
+titleString = ['Power Loss Normalized to Mean Power Capture With ',ERUstr,':',newline,...
+            varLabel,' Rate of Pressure Change Compared to Limit',newline,...
+            'Sea State ',num2str(SS)];
 title(titleString,...
 'Interpreter','latex','FontSize',fontSize,'fontname','Times')
 
@@ -428,3 +488,62 @@ leg.FontName = 'Times';
 rect = [0.5, -0.2, 0.25, 0.15];
 % set(leg, 'Position', rect)
 set(leg, 'Location', 'best')
+xLim = xlim;
+xlim([0 xLim(2)])
+yLim = ylim;
+ylim([0 yLim(2)])
+
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% distribution of accumulators versus total volume
+iax = 2;
+ax(iax) = subplot(n_plots,1,iax);
+ax(iax).FontName = 'times';
+ax(iax).FontSize = fontSize-1;
+
+for i = 1:N
+    iVar = iDom(iDomStart(i):iDomStart(i+1)-1);
+    p(iax,i) = plot(XaxisVar(iVar),100*X_mesh(iVar));
+    p(iax,i).Color = [color(i,:)];
+    p(iax,i).Marker = markerType(i);
+    p(iax,i).MarkerSize = 5;
+    p(iax,i).LineWidth = 1.5;
+    hold on
+
+end
+
+xlabel('volume (1000L) ', ...
+'Interpreter','latex','FontSize',fontSize-1,'fontname','Times')
+ylabel('portion of volume at RO inlet (x100\%)', ...
+'Interpreter','latex','FontSize',fontSize-1,'fontname','Times')
+xLim = xlim;
+xlim([0 xLim(2)])
+yLim = ylim;
+ylim([0 yLim(2)])
+
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% distribution of accumulators versus total volume
+iax = 3;
+ax(iax) = subplot(n_plots,1,iax);
+ax(iax).FontName = 'times';
+ax(iax).FontSize = fontSize-1;
+
+for i = 1:N
+    iVar = iDom(iDomStart(i):iDomStart(i+1)-1);
+    p(iax,i) = plot(XaxisVar(iVar),sqrt(1e3)*kv_mesh(iVar));
+    p(iax,i).Color = [color(i,:)];
+    p(iax,i).Marker = markerType(i);
+    p(iax,i).MarkerSize = 5;
+    p(iax,i).LineWidth = 1.5;
+    hold on
+
+end
+
+xlabel('volume (1000L) ', ...
+'Interpreter','latex','FontSize',fontSize-1,'fontname','Times')
+ylabel('valve coefficient (L/s/kPa^{1/2})', ...
+'Interpreter','latex','FontSize',fontSize-1,'fontname','Times')
+xLim = xlim;
+xlim([0 xLim(2)])
+yLim = ylim;
+ylim([0 yLim(2)])
+
