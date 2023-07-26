51df283f51d4aba44d6f257e9ccdb3d59bbbd3eb
Jeremy Simmons <simmo536@umn.edu>
2023-07-26 15:08:34 -0500

    add study on low-pressure branch design, charge pump speed and accumulator volume

---
+++ study_refPTO_chargePumpAccum_wPassiveRV.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -l
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --mem=16gb
+#SBATCH -t 4:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=simmo536@umn.edu
+#SBATCH -p small
+#SBATCH -o %A_%a.out
+#SBATCH -e %A_%a.err
+
+cd ~/2023-NSF_CPS-wavePoweredRO
+module load matlab
+matlab -nodisplay -r \
+"iVar = ${SLURM_ARRAY_TASK_ID}; \
+SS = $SS; \
+study_refPTO_chargePumpAccum_wPassiveRV"
+
+# Commands to use
+# sbatch --export=SS=1 --array=1-140 ~/2023-NSF_CPS-wavePoweredRO/study_refPTO_chargePumpAccum_wPassiveRV.sh
+# dos2unix  study_refPTO_chargePumpAccum_wPassiveRV.sh
